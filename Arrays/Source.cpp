af0f0c63664b630e3cc028f5c841b8038ee9c025
Uliana29 <Uliana29@DESKTOP-I69RM28>
2023-04-04 15:47:48 +0300

    Encoding

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -69,13 +69,13 @@ void shiftRight(double arr[], const int n, int number_of_shifts);
 void shiftRight(char arr[], const int n, int number_of_shifts);
 void shiftRight(char arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
 void shiftRight(double arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
-void shiftRight(const int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
+void shiftRight(int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
 
 void UniqueRand(int arr[], const int n);
 void UniqueRand(char arr[], const int n);
-void UniqueRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
 void UniqueRand(char arr[ROWS][COLS], char ROWS, char COLS);
 void UniqueRand(int arr[ROWS][COLS], const int ROWS,const int COLS);
+void UniqueRand(double arr[ROWS][COLS], const int ROWS,const int COLS);
 
 
 void Search(int arr[], const int n);
@@ -585,9 +585,28 @@ void shiftRight(double arr[ROWS][COLS], const int ROWS, const int COLS, int numb
 {
 	
 }
-void shiftRight(const int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
+void shiftRight(int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
 {
-	shiftRight(arr, ROWS, COLS, ROWS * COLS + number_of_shifts);
+	for (int k = 0; k < number_of_shifts; k++)
+	{
+		int buffer = arr[ROWS - 1][COLS - 1];
+		for (int i = ROWS - 1; i > 1; i--)
+		{
+			for (int j = COLS - 1; j > 1; j--)
+			{
+				if (j == 0 && i != 0)
+				{
+					arr[i][j] = arr[i - 1][COLS];
+				}
+				else
+				{
+					arr[i][j] = arr[i][j - 1];						
+				}
+			}
+		}
+			arr[0][0] = buffer;
+	}
+
 }
 void Sort(int arr[], const int n)
 {
@@ -741,8 +760,24 @@ void UniqueRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
 }
 void UniqueRand(char arr[], const int n)
 {
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+}
+void UniqueRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
 
 }
+
 void Search(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -781,7 +816,7 @@ void Search(int arr[ROWS][COLS], const int ROWS, const int COLS)
 			{
 				for (int l = 0; l < (k == i ? j : COLS); l++)
 				{
-					if (arr[i][j] == [k][l])
+					if (arr[i][j] == arr[k][l])
 					{
 						met_before = true;
 						break;
